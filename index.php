4f79c11a7f13bbe5ae5d73d2d6f132eecce6b6af
Marcio Douglas <marciodouglas197@gmail.com>
2019-05-07 23:28:25 -0300

    Mudei o primeiro echo e adicionei o segundo

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
 
-<<<<<<< HEAD
+
 	echo "Olá mundo 456!";
 	
-	echo "olá udemy";
+	echo "olá udemy e hcode";
 ?> 
-=======
-	echo "Olá mundo 123!";
 
-	echo "Ola hcode!";
-?>
->>>>>>> 03d31b3c8f1f8d2a97475560879f70f938c673f6
